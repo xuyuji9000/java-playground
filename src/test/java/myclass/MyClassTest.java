a38693438203de81411fd84527c946f10da0f6f0
Karl Xu <karl.yogi.xu@gmail.com>
2019-01-13 22:28:31 +0800

    add first test case

---
+++ src/test/java/myclass/MyClassTest.java
@@ -0,0 +1,21 @@
+package myclass;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+
+public class MyClassTest {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        MyClass tester = new MyClass(); // MyClass is tested
+
+        // assert statements
+        assertEquals(0, tester.multiply(10, 0));
+        assertEquals(0, tester.multiply(0, 10));
+        assertEquals(0, tester.multiply(0, 0));
+    }
+}
